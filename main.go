fb15cc49adbb23d35133160045d8f0b8a4a5bb01
marvin <j123211@qq.com>
2022-07-25 15:50:05 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func setupRouter() *gin.Engine {
+	gin.DisableConsoleColor()
+	r := gin.Default()
+	r.Static("/calendar", "./files/calendar")
+	return r
+}
+
 func main() {
 	r := setupRouter()
 	r.Run(":8080")
